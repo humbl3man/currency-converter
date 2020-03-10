f62c970460192e6804c632366f879c191ddd9c87
Konstantin M <humbl3dev@gmail.com>
2020-03-10 08:49:22 -0700

    adds reset button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { CURRENCIES } from "./data/currencies";
 import { fetchRates } from "./api";
 
 import "./App.css";
 
-const rates = {};
 const currencies = Object.entries(CURRENCIES);
 
-async function convert(amount, from, to) {
-  if (!rates[from]) {
-    console.log(`no rate is available for ${from}. Fetching...`);
-    const newBaseRate = await fetchRates(from);
-    rates[from] = newBaseRate;
-  }
-
-  return amount * rates[from].rates[to];
-}
+const rates = {};
 
 function formatCurrency(rawAmount, to) {
   return Intl.NumberFormat("en-US", { style: "currency", currency: to }).format(
@@ -28,6 +19,7 @@ function App() {
   const [toSelect, setToSelect] = useState("EUR");
   const [fromAmount, setFromAmount] = useState("");
   const [result, setResult] = useState("");
+  const inputRef = useRef(null);
 
   function handleFromSelect(e) {
     setFromSelect(e.target.value);
@@ -40,6 +32,15 @@ function App() {
   }
 
   useEffect(() => {
+    async function convert(amount, from, to) {
+      if (!rates[from]) {
+        console.log(`no rate is available for ${from}. Fetching...`);
+        const newBaseRate = await fetchRates(from);
+        rates[from] = newBaseRate;
+      }
+
+      return amount * rates[from].rates[to];
+    }
     convert(fromAmount, fromSelect, toSelect).then(result => {
       setResult(formatCurrency(result, toSelect));
     });
@@ -52,7 +53,6 @@ function App() {
       <form
         onSubmit={async e => {
           e.preventDefault();
-          await convert(100, "USD", "CAD");
         }}
       >
         <div className="mb-3">
@@ -61,6 +61,7 @@ function App() {
           </label>
           <input
             id="convertAmount"
+            ref={inputRef}
             value={fromAmount}
             onChange={handleAmountChange}
             placeholder={`Enter ${fromSelect} value`}
@@ -77,7 +78,7 @@ function App() {
             <div className="relative">
               <select
                 id="from"
-                defaultValue={fromSelect}
+                value={fromSelect}
                 onChange={handleFromSelect}
                 className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
               >
@@ -107,7 +108,7 @@ function App() {
             <div className="relative">
               <select
                 id="to"
-                defaultValue={toSelect}
+                value={toSelect}
                 onChange={handleToSelect}
                 className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
               >
@@ -131,6 +132,22 @@ function App() {
             </div>
           </div>
         </div>
+
+        <div className="mt-5 flex justify-end">
+          <button
+            type="button"
+            onClick={() => {
+              setFromAmount("");
+              setFromSelect("USD");
+              setToSelect("EUR");
+              inputRef.current.focus();
+            }}
+            className="bg-red-500 hover:bg-red-400 text-white font-bold py-1 px-4 rounded"
+          >
+            Reset
+          </button>
+        </div>
+
         <div className="mt-8 py-5 border-t border-gray-300">
           <p>
             <strong>Result:</strong> {result}
